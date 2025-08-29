412655b1fe1185ddac04a82fc3cc843e1acd9c1a
417MahirRahman <35mahirrahman@gmail.com>
2025-08-29 22:12:27 +0600

    added call history function

--- index.js
+++ index.js
@@ -1,46 +1,150 @@
-// Call Function 
+let avBalance = parseInt(document.getElementById("available-balance").innerText);
+let History = [];
+
+// Call Function
 function showService(serviceName) {
 
-    let avBalance = parseInt(document.getElementById("available-balance").innerText);
-    
     if(avBalance < 20){
-        alert("You Don't have coin for call !!!");
+        alert("You don't have enough coin for call !!!");
     }else{
         alert("Calling" +" "+serviceName);
         avBalance -= 20;
+        callHistory();
     }
 
     document.getElementById("available-balance").innerText = avBalance;
 }
 
+//Call History
+function callHistory(){
+    let history_container = document.getElementById("Call-History")
+    history_container.innerHTML = "";
+
+    for(let data of History){
+         let div = document.createElement("div")
+        div.innerHTML = `<div class="flex justify-between items-center bg-[gray]/7 mb-2 p-2 lg:mb-3 lg:p-5 rounded-xl">
+                            <div>
+                                <h1 class="text-[10px] font-bold lg:text-lg">${data.Name}</h1>
+                                <p class="text-[10px] font-medium lg:text-lg text-[gray]">${data.number}</p>
+                            </div>
+                            <div class="date">
+                                <p class="text-[10px] font-medium lg:text-lg text-[gray]">${data.date}</p>
+                            </div>
+                        </div>`
+
+        history_container.appendChild(div)               
+    }   
+}
+
+//Clear-History
+document.getElementById("Clear-btn").addEventListener("click",function(e){
+    e.preventDefault()
+    History = []
+    document.getElementById("Call-History").innerHTML = ""
+})
+
 //Event Listener for call function
     document.getElementById("National Emergency").addEventListener("click", function(e){ 
-        e.preventDefault;
-        showService("National Emergency service 999..."); });
+        e.preventDefault();
+
+        const data = {
+            Name : "National Emergency Number",
+            number : 999,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("National Emergency service 999...");
+    });
+
+
     document.getElementById("police").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Police Helpline service 999..."); });
+         
+        const data = {
+            Name : "Police Helpline Number",
+            number : 999,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Police Helpline service 999...");
+    });
     document.getElementById("fire service").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Fire service 999..."); });
+        
+        const data = {
+            Name : "Fire Service Number",
+            number : 999,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Fire service 999...");
+    });
     document.getElementById("Ambulance").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Ambulance service 1994-999999..."); });
+        
+        const data = {
+            Name : "Ambulance Service",
+            number : "1994-999999",
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Ambulance service 1994-999999...");
+    });
     document.getElementById("Women & Child").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Women & Child Helpline service 109..."); });
+
+        const data = {
+            Name : "Women & Child Helpline",
+            number : 109,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Women & Child Helpline service 109...");
+     });
     document.getElementById("Anti Corruption").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Anti-Corruption Helpline service 106..."); });
+
+        const data = {
+            Name : "Anti-Corruption Helpline",
+            number : 106,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Anti-Corruption Helpline service 106...");
+    });
     document.getElementById("Electricity").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Electricity Helpline service 16216..."); });
+
+        const data = {
+            Name : "Electricity Helpline",
+            number : 16216,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Electricity Helpline service 16216...");
+     });
     document.getElementById("Brac").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Brac Helpline service 16445..."); });
+
+        const data = {
+            Name : "Brac Helpline",
+            number : 16445,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Brac Helpline service 16445...");
+     });
     document.getElementById("Train").addEventListener("click", function(e){ 
         e.preventDefault;
-        showService("Bangladesh Railway Helpline service 163..."); });
+
+        const data = {
+            Name : "Bangladesh Railway Helpliner",
+            number : 163,
+            date : new Date().toLocaleTimeString()
+        }
+        History.push(data)
+        showService("Bangladesh Railway Helpline service 163...");
+     });
 
 
 //Event Listener for like function
@@ -75,4 +179,4 @@ for(let i=0; i<btn.length; i++){
         })
     })
 }
-         
+
